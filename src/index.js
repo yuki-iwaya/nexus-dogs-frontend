1d0af631ad1021dbc8de22605145939001f86e48
yuki-iwaya <yu0ki1iwa1ya4@gmail.com>
2025-07-28 17:30:57 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import './styles/base.css';
+import './styles/button.css';
+import './styles/card.css';
+import './styles/form.css';
 import reportWebVitals from './reportWebVitals';
+import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
+
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
+  return token ? children : <Navigate to="/login" />;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route
+      path="/dashboard"
+      element={
+        <PrivateRoute>
+          <Dashboard />
+        </PrivateRoute>
+      }
+    />
+  </Routes>
+</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
